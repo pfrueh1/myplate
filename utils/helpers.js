7b41aa0b628b4d770d296ff1903da3e59d342f71
Jamie Williams <jamwil1226@yahoo.com>
2021-11-27 19:14:30 -0500

    installed bcrypt, connect-session-sequelize, express-session, express-handlebars, set up all front end files

---
+++ utils/helpers.js
@@ -0,0 +1,47 @@
+const upload = () => {
+    if(err) {
+        res.render('./layouts/main.handlebars', {
+            msg: err
+        });
+    } else {
+        if(req.file === undefined){
+            res.render('./layouts/main.handlebars', {
+                msg: 'Error: No File Selected!'
+            });
+        } else {
+            console.log(req.file);
+            res.render('./layouts/main.handlebars', {
+                msg: 'File Uploaded!',
+                file: `uploads/${req.file.filename}`
+            });
+            const image = `uploads/${req.file.filename}`
+                return image;
+        }
+        
+    }
+};
+
+// module.exports = {
+//     format_date: date => {
+//       return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+//         date
+//       ).getFullYear()}`;
+//     },
+//     format_url: url => {
+//       return url
+//         .replace('http://', '')
+//         .replace('https://', '')
+//         .replace('www.', '')
+//         .split('/')[0]
+//         .split('?')[0];
+//     },
+//     format_plural: (word, amount) => {
+//       if (amount !== 1) {
+//         return `${word}s`;
+//       }
+  
+//       return word;
+//     }
+// };
+
+module.exports = upload;
