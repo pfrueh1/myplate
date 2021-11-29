2517f9d92bc1530f7c6dbd1a4fb1ab93bc73ac59
Jamie Williams <jamwil1226@yahoo.com>
2021-11-29 16:44:39 -0500

    sailors font & separate css for handlebars pages

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -42,7 +42,10 @@ router.get('/', withAuth, (req, res) => {
 });
 
 router.get('/edit/:id', withAuth, (req, res) => {
-  Post.findByPk(req.params.id, {
+  Post.findOne({
+    where: {
+      id: req.params.id
+  },
     attributes: [
       'id',
       'title',
